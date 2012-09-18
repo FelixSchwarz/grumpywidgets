6f41aa0b8f19a5614a7df7d2fc45fb2bf0456529
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-09-18 16:29:44 +0200

    forms can render themselves (though no children)

--- grumpywidgets/forms/api.py
+++ grumpywidgets/forms/api.py
@@ -5,10 +5,27 @@
 from grumpywidgets.api import Widget
 
 
-__all__ = ['InputWidget']
+__all__ = ['InputWidget', 'Form']
 
 class InputWidget(Widget):
     validator = None
     name = None
     label = None
+
+
+class Form(InputWidget):
+    url = ''
+    method = 'POST'
+    charset = 'UTF-8'
+    
+    def display(self, value):
+        form = '<form '
+        if self.id is not None:
+            form += 'id="%s" ' % unicode(self.id)
+        if self.name is not None:
+            form += 'name="%s" ' % unicode(self.name)
+        if self.css_classes:
+            form += 'class="%s" ' % ' '.join(self.css_classes)
+        form += 'action="%s" method="%s" accept-charset="%s"></form>' % (self.url, self.method, self.charset)
+        return form
 
---
+++ grumpywidgets/forms/tests/form_attributes_test.py
@@ -0,0 +1,52 @@
+# This file is a part of GrumpyForms.
+# The source code contained in this file is licensed under the MIT license.
+# See LICENSE.txt in the main project directory, for more information.
+
+
+from grumpywidgets.forms.api import Form
+from grumpywidgets.lib.pythonic_testcase import *
+
+
+class FormAttributesTest(PythonicTestCase):
+    def test_can_render_empty_form(self):
+        class EmptyForm(Form):
+            pass
+        assert_equals('<form action="" method="POST" accept-charset="UTF-8"></form>', 
+                      EmptyForm().display(None))
+    
+    def test_can_render_url(self):
+        class URLForm(Form):
+            url = 'http://foo.example/submit.php'
+        assert_equals('<form action="http://foo.example/submit.php" method="POST" accept-charset="UTF-8"></form>', 
+                      URLForm().display(None))
+    
+    def test_can_render_method(self):
+        class MethodForm(Form):
+            method = 'PUT'
+        assert_equals('<form action="" method="PUT" accept-charset="UTF-8"></form>', 
+                      MethodForm().display(None))
+    
+    def test_can_render_form_name(self):
+        class NamedForm(Form):
+            name = 'named_form'
+        assert_equals('<form name="named_form" action="" method="POST" accept-charset="UTF-8"></form>',
+                      NamedForm().display(None))
+    
+    def test_can_render_form_id(self):
+        class IDForm(Form):
+            id = 'form-id'
+        assert_equals('<form id="form-id" action="" method="POST" accept-charset="UTF-8"></form>', 
+                      IDForm().display(None))
+    
+    def test_can_render_charset(self):
+        class CharsetForm(Form):
+            charset = 'ISO-8859-1'
+        assert_equals('<form action="" method="POST" accept-charset="ISO-8859-1"></form>', 
+                      CharsetForm().display(None))
+    
+    def test_can_render_css_classes(self):
+        class StyledForm(Form):
+            css_classes = ('foo', 'bar')
+        assert_equals('<form class="foo bar" action="" method="POST" accept-charset="UTF-8"></form>', 
+                      StyledForm().display(None))
+

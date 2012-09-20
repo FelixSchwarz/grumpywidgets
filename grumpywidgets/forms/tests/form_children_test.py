5f4b2532a48c17894a02e258dd8a1365f2f1bd84
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-09-20 14:57:54 +0200

    refactor form tests

--- grumpywidgets/forms/tests/form_children_test.py
+++ grumpywidgets/forms/tests/form_children_test.py
@@ -23,8 +23,7 @@ class FormChildrenRenderingTest(PythonicTestCase):
     def test_can_render_single_child(self):
         expected = '<div class="number-container fieldcontainer">' + \
             '<input type="text" name="number" /></div>'
-        self.assert_child_html(expected, self.form.display({}), 
-                               strip_container=False)
+        self.assert_child_html(expected, self.form.display(), strip_container=False)
     
     def test_can_pass_values_to_children(self):
         expected = '<input type="text" name="number" value="send" />'
@@ -45,18 +44,6 @@ class FormChildrenRenderingTest(PythonicTestCase):
             '<span class="fielderror">bad input</span>'
         self.assert_child_html(expected, self.form.display())
     
-    def child_html(self, container_tag, html):
-        regex_string = '<%(tag)s[^>]*>\s*(.+)\s*</%(tag)s>' % dict(tag=container_tag)
-        match = re.search(regex_string, html.replace('\n', ''))
-        assert_not_none(match)
-        return match.group(1)
-    
-    def assert_child_html(self, expected, rendered_form, strip_container=True):
-        child_html = self.child_html('form', rendered_form)
-        if strip_container:
-            child_html = self.child_html('div', rendered_form)
-        assert_equals(expected, child_html)
-    
     def test_can_redisplay_previous_values_after_failed_validation(self):
         assert_raises(InvalidDataError, lambda: self.form.validate({'number': 'abc'}))
         expected = u'<input type="text" name="number" value="abc" />' + \
@@ -72,25 +59,40 @@ class FormChildrenRenderingTest(PythonicTestCase):
     # --- child container ----------------------------------------------------
     
     def test_container_contains_css_class_with_child_name(self):
-        expected = '<div class="number-container fieldcontainer"></div>'
-        self.assert_container_html(expected, self.form.display({}))
+        container_html = self.child_container_html(self.form.display({}))
+        self.assert_contains('class="number-container fieldcontainer"', container_html)
     
     def test_can_render_view_only_children(self):
         self.form.children = [Label(value='foo'), ]
         
         self.assert_child_html('<label>foo</label>', self.form.display())
-        expected = '<div class="fieldcontainer"></div>'
-        self.assert_container_html(expected, self.form.display({}))
+        container_html = self.child_container_html(self.form.display({}))
+        self.assert_contains('class="fieldcontainer"', container_html)
     
     # --- helpers -------------------------------------------------------------
     
-    def container_html(self, container_tag, html):
-        regex_string = '(<%(tag)s[^>]*>)\s*(?:.+)\s*(</%(tag)s>)' % dict(tag=container_tag)
+    def _split_html(self, container_tag, html):
+        regex_string = '(<%(tag)s[^>]*>)\s*(.+)\s*(</%(tag)s>)' % dict(tag=container_tag)
         match = re.search(regex_string, html.replace('\n', ''))
         assert_not_none(match)
-        return match.group(1) + match.group(2)
+        return (match.group(1) + match.group(3), match.group(2))
+    
+    def child_html(self, container_tag, html):
+        return self._split_html(container_tag, html)[1]
+    
+    def assert_child_html(self, expected, rendered_form, strip_container=True):
+        child_html = self.child_html('form', rendered_form)
+        if strip_container:
+            child_html = self.child_html('div', rendered_form)
+        assert_equals(expected, child_html)
+    
+    def container_html(self, container_tag, html):
+        return self._split_html(container_tag, html)[0]
+    
+    def child_container_html(self, rendered_form):
+        child_html = self.child_html('form', rendered_form)
+        return self.container_html('div', child_html)
     
     def assert_container_html(self, expected, rendered_form):
-        child_html = self.child_html('form', rendered_form)
-        assert_equals(expected, self.container_html('div', child_html))
+        assert_equals(expected, self.child_container_html(rendered_form))
 

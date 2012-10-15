c8f5935a4c849f96230d7481f5d3346d29394ba9
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-10-15 12:50:57 +0200

    implement email field

--- grumpywidgets/forms/fields/inputfields.py
+++ grumpywidgets/forms/fields/inputfields.py
@@ -2,25 +2,29 @@
 # The source code contained in this file is licensed under the MIT license.
 # See LICENSE.txt in the main project directory, for more information.
 
-from pycerberus.validators import StringValidator
+from pycerberus.validators import EmailAddressValidator, StringValidator
 
 from grumpywidgets.forms.api import InputWidget
 
 
-__all__ = ['PasswordField', 'HiddenField', 'TextField']
+__all__ = ['EmailField', 'HiddenField', 'PasswordField', 'TextField']
 
 class TextLikeInputField(InputWidget):
     template = 'textlike_input_field.jinja2'
     validator = StringValidator()
 
 
-class TextField(TextLikeInputField):
-    type = 'text'
-    validator = StringValidator(required=False)
+class EmailField(TextLikeInputField):
+    type = 'email'
+    validator = EmailAddressValidator()
 
 class HiddenField(TextLikeInputField):
     type = 'hidden'
 
 class PasswordField(TextLikeInputField):
     type = 'password'
+
+class TextField(TextLikeInputField):
+    type = 'text'
+    validator = StringValidator(required=False)
 
---
+++ grumpywidgets/forms/fields/tests/email_field_test.py
@@ -0,0 +1,29 @@
+# This file is a part of GrumpyWidgets.
+# The source code contained in this file is licensed under the MIT license.
+# See LICENSE.txt in the main project directory, for more information.
+
+from grumpywidgets.forms.fields import EmailField
+from grumpywidgets.forms.fields.test_support import InputFieldTestTemplate
+from grumpywidgets.lib.pythonic_testcase import *
+
+
+class EmailFieldFieldTest(InputFieldTestTemplate):
+    __test__ = True
+    
+    field_class = EmailField
+    
+    def test_has_email_validator_by_default(self):
+        text_field = EmailField()
+        
+        assert_not_none(text_field.validator)
+        self.assert_error(text_field, [])
+        self.assert_error(text_field, '')
+        self.assert_error(text_field, 'foo')
+        assert_equals('foo@domain.example', 
+                      text_field.validate('foo@domain.example').value)
+    
+    def assert_error(self, widget, input_):
+        context = widget.validate(input_)
+        assert_true(context.contains_errors())
+
+

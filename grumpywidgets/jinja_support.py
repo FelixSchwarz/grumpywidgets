8402d423776e0542fc1a90401113e3b3a56cd95b
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-09-20 12:06:22 +0200

    catch ImportError if Jinja2 is not available

--- grumpywidgets/jinja_support.py
+++ grumpywidgets/jinja_support.py
@@ -2,14 +2,19 @@
 # The source code contained in this file is licensed under the MIT license.
 # See LICENSE.txt in the main project directory, for more information.
 
-
-from jinja2 import Environment, PackageLoader, Template
-from jinja2.loaders import FileSystemLoader
+try:
+    from jinja2 import Environment, PackageLoader, Template
+    from jinja2.loaders import FileSystemLoader
+    is_jinja2_available = True
+except ImportError:
+    is_jinja2_available = False
 
 
 __all__ = ['render_jinja_template']
 
 def render_jinja_template(template, template_variables, template_path):
+    if not is_jinja2_available:
+        raise ValueError('Jinja2 not available')
     if hasattr(template, 'read'):
         template_ = Template(template.read())
         template.seek(0)

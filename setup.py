c1b1cdd9932356e7ae275e67e18e9643b87f5fb0
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-12-07 14:25:24 +0100

    move project metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,39 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import re
+from setuptools import setup
 
-import setuptools
+setup()
 
-
-def requires_from_file(filename):
-    requirements = []
-    with open(filename, 'r') as requirements_fp:
-        for line in requirements_fp.readlines():
-            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
-            if match:
-                requirements.append(match.group(1))
-    return requirements
-
-setuptools.setup(
-    name = 'GrumpyWidgets',
-    version = '0.2.99.20170418',
-
-    description = 'A grumpy widget library',
-    license = 'MIT',
-    author = 'Felix Schwarz',
-    author_email = 'felix.schwarz@oss.schwarz.eu',
-    install_requires=requires_from_file('requirements.txt'),
-
-    namespace_packages = [
-        'grumpyforms',
-        'grumpyforms.ext',
-        'grumpywidgets',
-        'grumpywidgets.ext',
-    ],
-    packages=setuptools.find_packages(),
-    include_package_data = True,
-    tests_require = ['nose'],
-    test_suite = 'nose.collector',
-    zip_safe=False,
-)
